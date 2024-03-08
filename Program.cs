68cd55a4ed576ab9424731e3908da260ecec0283
Matthew Beale <129032908+mttble@users.noreply.github.com>
2024-03-08 13:16:20 +1030

    Part of auth implemented

--- Program.cs
+++ Program.cs
@@ -9,4 +9,10 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddMsalAuthentication(options =>
+{
+    builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+    options.ProviderOptions.LoginMode = "redirect";
+});
+
 await builder.Build().RunAsync();
